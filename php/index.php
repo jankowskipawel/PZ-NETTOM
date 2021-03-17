94d4f4b7d0f7c90a8344878f4aa49fb146abdc48
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-03-17 17:13:50 +0100

    Unify php and python time tests

--- php/index.php
+++ php/index.php
@@ -2,27 +2,39 @@
 
 require_once 'database.php';
 ini_set('memory_limit', '1000M');
+$ram="1GB";
+echo '<div style="width: 100%; font-weight: bold; font-size: 16pt;">';
+echo "PHP (RAM: ".$ram.")<br><br>";
+echo '</div>';
 $startTime = new DateTime();
-echo "Czas rozpoczęcia: ".$startTime->format('H:i:s:u')."<br><br>";
-
+// echo "Czas rozpoczęcia: ".$startTime->format('H:i:s:u')."<br><br>";
+echo "Loading database...<br>";
 $result = $connection->query("SELECT * FROM covid ORDER BY location ASC");
 if($result==false) {
     throw new Exception($connection->error);
 }
 
-echo 'Wczytano rekordów: '.mysqli_num_rows($result).'<br><br>';
 //database - variable with whole database
 $database = array();
 while($rows = $result->fetch_row()) {
-
+    
     $database[] = $rows;
 }
-echo $database[0][3];
+echo 'Loaded '.count($database).' rows ('.count($database[0]).' columns each).<br>';
+// echo $database[0][3];
 
 $stopTime = new DateTime();
-echo "Czas zakończenia: ".$stopTime->format('H:i:s:u')."<br><br>";
 
 $difference = $startTime->diff($stopTime);
-echo "Czas trwania: ".$difference->format('%Im %Ss %Fms');
+echo 'Elapsed time (database load): '.$difference->format('%Im %Ss %Fms').' (Started: '.$startTime->format('H:i:s:u').', Finished: '.$stopTime->format('H:i:s:u').')<br><br>';
 
+echo "Executing script...<br>";
+$startTime = new DateTime();
+
+//############insert script here############
+
+
+$stopTime = new DateTime();
+$difference = $startTime->diff($stopTime);
+echo 'Elapsed time (script execution): '.$difference->format('%Im %Ss %Fms').' (Started: '.$startTime->format('H:i:s:u').', Finished: '.$stopTime->format('H:i:s:u').')<br><br>';
 die();
--- python/time-test.py
+++ python/time-test.py
@@ -1,22 +1,42 @@
 #!/var/www/html/cgi-enabled/env/bin/python3
 import pymysql
 import datetime
+ram = "1GB"
 print("Content-type: text/html\n\n")
 print("<html>\n<body>")
-print( "<div style=\"width: 100%; font-size: 24px; text-align: center;\">")
+print( "<div style=\"width: 100%; font-size: 12pt;\">")
 
 db = pymysql.connect(host='192.168.0.27',
                              user='root',
                              password='PzNettom2021!',
-                             database='CSV_DB' )
+                             database='CSV_DB',
+                             port=3306)
 cursor = db.cursor()
+print( "<div style=\"width: 100%; font-size: 12pt;\">")
+print( "<div style=\"width: 100%; font-weight: bold; font-size: 16pt;\">")
+print(f"PYTHON (RAM: {ram})<br><br>")
+print( "</div>")
+
+print("Loading database...<br>")
 datestart = datetime.datetime.now()
 sql = "SELECT * FROM covid ORDER BY location ASC"
 cursor.execute(sql)
-#result = cursor.fetchone()
-#print(result)
+rows = cursor.fetchall()
+database = []
+for row in rows:
+  database.append(row)
 db.close()
 dateend = datetime.datetime.now()
-print(f"start: {datestart}, end: {dateend}")
+print(f"Loaded {len(database)} rows ({len(database[0])} columns each).<br>")
+#database is List[List[str]]
+print(f"Elapsed time (database load): {dateend-datestart}   (Started: {datestart}, Finished: {dateend})<br><br>")
+print("Executing script...<br>")
 
+datestart = datetime.datetime.now()
+
+############insert script here############
+#print(database[0])
+
+dateend = datetime.datetime.now()
+print(f"Elapsed time (script execution): {dateend-datestart}   (Started: {datestart}, Finished: {dateend})")
 print("</div>\n</body>\n</html>")
