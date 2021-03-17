15c4efd436065c5539ae3adacfaf3d5e85a38379
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-03-18 00:59:44 +0100

    Remove unused comments

--- php/index.php
+++ php/index.php
@@ -8,21 +8,19 @@ echo "PHP (RAM: ".$ram.")<br><br>";
 echo '</div>';
 $startTime = new DateTime();
 $totalTime = new DateTime('00:00');
-// echo "Czas rozpoczęcia: ".$startTime->format('H:i:s:u')."<br><br>";
 echo "Loading database...<br>";
 $result = $connection->query("SELECT * FROM covid ORDER BY location ASC");
 if($result==false) {
     throw new Exception($connection->error);
 }
 
-//database - array[array[str]]
+//database is array[array[str]]
 $database = array();
 while($rows = $result->fetch_row()) {
     
     $database[] = $rows;
 }
 echo 'Loaded '.count($database).' rows ('.count($database[0]).' columns each).<br>';
-// echo $database[0][3];
 
 $stopTime = new DateTime();
 
